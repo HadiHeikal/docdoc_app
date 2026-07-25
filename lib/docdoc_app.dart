54c148a4ffa1deed344178983a2230694e35b026
HadiHeikal <hadiiheikal@gmail.com>
2026-07-25 07:00:33 +0300

    Refactor DocdocApp to integrate ScreenUtil for responsive design and update constructor to require AppRouter

--- lib/docdoc_app.dart
+++ lib/docdoc_app.dart
@@ -1,17 +1,24 @@
+import 'package:docdoc_app/config/routing/app_router.dart';
+import 'package:docdoc_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocdocApp extends StatelessWidget {
-  const DocdocApp({super.key});
+  // The root of our application.
+  final AppRouter appRouter;
+  const DocdocApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Docdoc App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        title: 'Docdoc App',
+        theme: AppTheme.lightTheme,
+        home: const Placeholder(), // Replace with your actual home widget
       ),
-      home: const Placeholder(), // Replace with your actual home widget
     );
   }
 }
