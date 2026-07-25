0e23a12cb40a6c6affdfced4bdd79cc1a4964dd8
HadiHeikal <hadiiheikal@gmail.com>
2026-07-25 20:36:44 +0300

    Add constants for app images and UI strings; update text styles for responsive design

---
+++ lib/core/constants/ui_strings.dart
@@ -0,0 +1,10 @@
+abstract class UiStrings {
+  const UiStrings._();
+
+  static const String appName = 'Docdoc';
+  static const String onboardingTitle = 'Best Doctor\nAppointment App';
+  static const String onboardingSubtitle =
+      'Manage and schedule all of your medical appointments easily '
+      'with Docdoc to get a new experience.';
+  static const String getStarted = 'Get Started';
+}
