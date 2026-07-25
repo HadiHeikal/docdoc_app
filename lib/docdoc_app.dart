c8d78ecbfeffb5f5a574d567726732bda80bddf9
HadiHeikal <hadiiheikal@gmail.com>
2026-07-25 13:38:28 +0300

    Refactor project structure: remove README.md, integrate ThemeCubit for theme management, and update dependencies in pubspec.yaml

--- lib/docdoc_app.dart
+++ lib/docdoc_app.dart
@@ -1,6 +1,8 @@
 import 'package:docdoc_app/config/routing/app_router.dart';
 import 'package:docdoc_app/core/theme/app_theme.dart';
+import 'package:docdoc_app/core/theme/cubit/theme_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocdocApp extends StatelessWidget {
@@ -10,14 +12,24 @@ class DocdocApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 800),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      child: MaterialApp(
-        title: 'Docdoc App',
-        theme: AppTheme.lightTheme,
-        home: const Placeholder(), // Replace with your actual home widget
+    return BlocProvider(
+      create: (_) => ThemeCubit(),
+      child: ScreenUtilInit(
+        designSize: const Size(360, 800),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: BlocBuilder<ThemeCubit, ThemeMode>(
+          builder: (context, themeMode) {
+            return MaterialApp(
+              title: 'Docdoc App',
+              theme: AppTheme.lightTheme,
+              darkTheme: AppTheme.darkTheme,
+              themeMode: themeMode,
+              debugShowCheckedModeBanner: false,
+              home: const Placeholder(), // Replace with your actual home widget
+            );
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:docdoc_app/config/routing/app_router.dart';
-import 'package:flutter/material.dart';
 import 'package:docdoc_app/docdoc_app.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory(
+            (await getApplicationDocumentsDirectory()).path,
+          ),
+  );
+
   runApp(DocdocApp(appRouter: AppRouter()));
 }
