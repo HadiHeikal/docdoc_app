7b9ef57649e6aa67e89a30b945325deeb4a28edc
HadiHeikal <hadiiheikal@gmail.com>
2026-07-25 06:30:40 +0300

    Implement initial views and routing for Home, Profile, Onboarding, Login, and Register

--- lib/config/routing/app_router.dart
+++ lib/config/routing/app_router.dart
@@ -1,29 +1,24 @@
 import 'package:docdoc_app/config/routing/routes.dart';
+import 'package:docdoc_app/features/auth/presentation/login/views/login_view.dart';
+import 'package:docdoc_app/features/auth/presentation/register/views/register_view.dart';
+import 'package:docdoc_app/features/home/presentation/home_view.dart';
+import 'package:docdoc_app/features/onboarding/presentation/onboarding_view.dart';
+import 'package:docdoc_app/features/profile/presentation/profile_view.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   MaterialPageRoute<dynamic> generateRoutes(RouteSettings settings) {
     switch (settings.name) {
       case Routes.homeView:
-        return MaterialPageRoute(
-          builder: (_) => const Placeholder(),
-        ); // Replace with your actual home widget
+        return MaterialPageRoute(builder: (_) => const HomeView());
       case Routes.profileView:
-        return MaterialPageRoute(
-          builder: (_) => const Placeholder(),
-        ); // Replace with your actual profile widget
+        return MaterialPageRoute(builder: (_) => const ProfileView());
       case Routes.onboardingView:
-        return MaterialPageRoute(
-          builder: (_) => const Placeholder(),
-        ); // Replace with your actual onboarding widget
+        return MaterialPageRoute(builder: (_) => const OnboardingView());
       case Routes.loginView:
-        return MaterialPageRoute(
-          builder: (_) => const Placeholder(),
-        ); // Replace with your actual login widget
+        return MaterialPageRoute(builder: (_) => const LoginView());
       case Routes.registerView:
-        return MaterialPageRoute(
-          builder: (_) => const Placeholder(),
-        ); // Replace with your actual register widget
+        return MaterialPageRoute(builder: (_) => const RegisterView());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
