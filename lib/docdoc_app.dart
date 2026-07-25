0cf4a3e21666ad9dfef57119cfdb630c428d81c2
HadiHeikal <hadiiheikal@gmail.com>
2026-07-25 14:28:52 +0300

    Add routing configuration and theme setup in DocdocApp

--- lib/docdoc_app.dart
+++ lib/docdoc_app.dart
@@ -1,4 +1,5 @@
 import 'package:docdoc_app/config/routing/app_router.dart';
+import 'package:docdoc_app/config/routing/routes.dart';
 import 'package:docdoc_app/core/theme/app_theme.dart';
 import 'package:docdoc_app/core/theme/cubit/theme_cubit.dart';
 import 'package:flutter/material.dart';
@@ -22,9 +23,14 @@ class DocdocApp extends StatelessWidget {
           builder: (context, themeMode) {
             return MaterialApp(
               title: 'Docdoc App',
+              // Set the light and dark themes,
+              //and the theme mode based on the ThemeCubit state
               theme: AppTheme.lightTheme,
               darkTheme: AppTheme.darkTheme,
               themeMode: themeMode,
+              // Set the initial route and the route generator
+              onGenerateRoute: appRouter.generateRoutes,
+              initialRoute: Routes.onboardingView,
               debugShowCheckedModeBanner: false,
               home: const Placeholder(), // Replace with your actual home widget
             );
